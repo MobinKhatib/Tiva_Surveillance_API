de22d5a887cc0169118b9fca0dcd0e25beec7173
Mobin Khatib <mbnkhatib@gmail.com>
2025-05-03 14:07:09 +0430

    commit message - Salar

--- config.py
+++ config.py
@@ -1,9 +1,20 @@
 # config.py - Centralized settings
 
+import os
+
+# Get the directory where config.py is located
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CAR_MODEL_PATH = os.path.join(BASE_DIR, "model", "yolo11n.pt")
+PLATE_MODEL_PATH = os.path.join(BASE_DIR, "model", "best.pt")
+CHAR_MODEL_PATH = os.path.join(BASE_DIR, "model", "plate_character_recognizer_1.pt")
+
+"""
 ###Model Paths ###
-CAR_MODEL_PATH="E:/University/Semesters/Work/Tiva_Surveillance_API/model/best.pt" 
+CAR_MODEL_PATH="E:/University/Semesters/Work/Tiva_Surveillance_API/model/yolo11n.pt" 
 PLATE_MODEL_PATH="E:/University/Semesters/Work/Tiva_Surveillance_API/model/best.pt"
 CHAR_MODEL_PATH="E:/University/Semesters/Work/Tiva_Surveillance_API/model/plate_character_recognizer_1.pt"
+"""
 ### General Settings ###
 DEVICE = 'cpu'
 FPS = 30  # Default FPS
@@ -16,7 +27,7 @@ PLATE_REGION = [(100, 200), (1250, 720)]  # Rectangle for plate detection
 ENABLE_PLATE_CROP = True
 ENABLE_PLATE = True  # Set to False to disable plate detection
 CONFIDENCE_THRESHOLD = 0.2  # Minimum confidence to save detected plates
-ENABLE_WATERMARK = True  # Set False to disable watermark
+ENABLE_WATERMARK = False  # Set False to disable watermark
 WATERMARK_COLOR = "black"  # Options: "white", "black"
 # Real Address
 ENABLE_ADDRESS = True
@@ -30,6 +41,7 @@ DATE = ""
 # Location
 ENABLE_LOCATION = True
 LOCATION =  ""
-
 # date on the text
 #ENABLE_DATE_TEXT = False
+
+# 
